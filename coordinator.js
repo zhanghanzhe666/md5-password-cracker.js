80be9156bb799fe3526c6db4a5615976ce461230
张涵哲 <146498764+zhanghanzhe666@users.noreply.github.com>
2024-11-23 09:47:44 +0800

    Update coordinator.js

--- coordinator.js
+++ coordinator.js
@@ -1,100 +1,90 @@
-var numWorkers = 8 // NOTE: can't set this to be more than 8 without fixing the way numbers are carried
-var workers = []
-var startTime = +new Date
+var numWorkers = 8; // NOTE: can't set this to be more than 8 without fixing the way numbers are carried
+var workers = [];
+var startTime = +new Date;
 
-for (var i = 0; i < numWorkers; i++) {
+// 获取用户输入的哈希值
+var userInputHash = prompt("Please enter the hash to crack:");
 
-  // Create worker
-  var worker = new Worker('worker.js')
-  workers.push(worker)
+// 如果用户输入了哈希值，则进行密码破解
+if (userInputHash) {
+  var hashToCrack = userInputHash;
 
-  // Message handler
-  worker.addEventListener('message', function (e) {
-    switch (e.data.cmd) {
-      case "status":
-        status(e.data.data, e.data.id)
-        break
+  for (var i = 0; i < numWorkers; i++) {
 
-      case "log":
-        log(e.data.data, e.data.id)
-        break
+    // Create worker
+    var worker = new Worker('worker.js');
+    workers.push(worker);
 
-      case "setRate":
-        status(addCommasToInteger(e.data.data) + " passwords/second", e.data.id)
-        break
+    // Message handler
+    worker.addEventListener('message', function (e) {
+      switch (e.data.cmd) {
+        case "status":
+          status(e.data.data, e.data.id);
+          break;
 
-      case "foundPassword":
-        log("FOUND PASSWORD: " + e.data.data)
+        case "log":
+          log(e.data.data, e.data.id);
+          break;
 
-        var totalTime = (+new Date - startTime) / 1000
-        log("TOTAL TIME: " + totalTime + " seconds")
+        case "setRate":
+          status(addCommasToInteger(e.data.data) + " passwords/second", e.data.id);
+          break;
 
-        workers.forEach(function(worker) {
-          worker.terminate()
-        })
-        log("Terminated all workers.")
-        break
+        case "foundPassword":
+          log("FOUND PASSWORD: " + e.data.data);
 
-      default:
-        log("Main page doesn't understand command " + e.data.cmd)
-        break
-    }
-  })
+          var totalTime = (+new Date - startTime) / 1000;
+          log("TOTAL TIME: " + totalTime + " seconds");
 
-  // Error handler
-  worker.addEventListener('error', function(e) {
-    log(['ERROR: Line ', e.lineno, ' in ', e.filename, ': ', e.message].join(''))
-  })
+          workers.forEach(function(worker) {
+            worker.terminate();
+          });
+          log("Terminated all workers.");
+          break;
 
-  // Set worker settings
-  worker.postMessage({ cmd: "setWorkerId", data: i })
-  worker.postMessage({ cmd: "setMaxPassLength", data: 5 })
-  worker.postMessage({ cmd: "setPassToCrack", data: "54d75975e615f0638b6181592a4d929f" })
+        default:
+          log("Main page doesn't understand command " + e.data.cmd);
+          break;
+      }
+    });
 
-  // Start worker
-  worker.postMessage({ cmd: "performCrack", data: {start: i, hop: numWorkers} })
-}
+    // Error handler
+    worker.addEventListener('error', function(e) {
+      log(['ERROR: Line ', e.lineno, ' in ', e.filename, ': ', e.message].join(''));
+    });
 
-status("Searching for password match for hash '54d75975e615f0638b6181592a4d929f'.")
-log("Testing uppercase, lowercase, and numbers.")
+    // Set worker settings
+    worker.postMessage({ cmd: "setWorkerId", data: i });
+    worker.postMessage({ cmd: "setMaxPassLength", data: 5 });
+    worker.postMessage({ cmd: "setPassToCrack", data: hashToCrack });
 
-
-// Helper functions
-
-function addCommasToInteger(x) {
-  x = parseInt(x) + ''
-  var rgx = /(\d+)(\d{3})/
-  while (rgx.test(x)) {
-    x = x.replace(rgx, '$1' + ',' + '$2')
+    // Start worker
+    worker.postMessage({ cmd: "performCrack", data: {start: i, hop: numWorkers} });
   }
-  return x
+
+  status("Searching for password match for hash '" + hashToCrack + "'.");
+  log("Testing uppercase, lowercase, and numbers.");
+
+  // Helper functions
+  function addCommasToInteger(x) {
+    x = parseInt(x) + '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x)) {
+      x = x.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x;
+  }
+
+  function status(msg, workerId) {
+    var prefix = workerId != null ? "Worker " + workerId + " status: " : "Main page status: ";
+    document.querySelector("#mainstatus").textContent = prefix + msg;
+  }
+
+  function log(msg, workerId) {
+    var prefix = workerId != null ? "Worker " + workerId + " says: " : "Main page says: ";
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(document.createTextNode(prefix + msg));
+    fragment.appendChild(document.createElement('br'));
+    document.querySelector("#mainlog").appendChild(fragment);
+  }
 }
-
-function status(msg, workerId) {
-  var prefix = workerId != null
-    ? "Worker " + workerId + " status: "
-    : "Main page status: "
-
-  var selector = workerId != null
-    ? "#worker" + workerId
-    : "#main"
-
-  document.querySelector(selector).textContent = prefix + msg
-}
-
-function log(msg, workerId) {
-  var prefix = workerId != null
-    ? "Worker " + workerId + " says: "
-    : "Main page says: "
-
-  var fragment = document.createDocumentFragment();
-  fragment.appendChild(document.createTextNode(prefix + msg));
-  fragment.appendChild(document.createElement('br'));
-
-  var selector = workerId != null
-    ? "#worker" + workerId + "log"
-    : "#mainlog"
-
-  document.querySelector(selector).appendChild(fragment)
-}
-
